cb7ee4e17a44c93440dd1e559f223ce2b164f8e4
AndreAmorim05git config --global user.email js.andreamorim@gmail.com <jsand@pop-os.localdomain>
2023-03-15 15:35:30 -0300

    Update folder permission order

--- iacl.sh
+++ iacl.sh
@@ -30,9 +30,9 @@ chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
-chmod 777 /publico
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
+chmod 777 /publico
 
 echo "Tarefa finalizada."
